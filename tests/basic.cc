f08a00f906ffa217d3f5c3169f3f5d421e45044f
Nicu Tofan <nicu.tofan@gmail.com>
2015-10-29 20:40:59 +0200

    Create basic object

--- tests/basic.cc
+++ tests/basic.cc
@@ -10,12 +10,12 @@ public:
     virtual ~TestUsage () {}
 
     void SetUp(){
-
+        inst_ = new DbStruct();
     }
     void TearDown(){
-
+        delete inst_;
     }
-
+    DbStruct * inst_;
 };
 
 TEST_F(TestUsage, initial_state) {
